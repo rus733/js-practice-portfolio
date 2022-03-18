6c84ed277f3d8d854db560e8cd9a8281fea38814
rus733 <9282716617@mail.ru>
2022-03-18 15:24:10 +0300

    add task maden

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,7 @@
-const tabButtons = document.querySelectorAll('.design-list__item'); //
+const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescriptions = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
+const title = document.querySelector('title');
 
 function changeContent(array, value) {
   array.forEach((element) => {
@@ -12,11 +13,19 @@ function changeContent(array, value) {
   });
 }
 
+function changeTitle(text) {
+  title.innerText = text;
+}
+
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener('click', (e) => {
     const dataValue = tabButton.dataset.tabsHandler;
+    const titleText = tabButton.innerText;
+
     changeContent(tabDescriptions, dataValue);
     changeContent(tabImages, dataValue);
+    changeTitle(titleText);
+
     tabButtons.forEach((btn) => {
       if (btn === e.target) {
         btn.classList.add('design-list__item_active');
