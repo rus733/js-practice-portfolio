f5724ded8c3c17cc0412e39aa067a435c4f97d42
rus733 <9282716617@mail.ru>
2022-03-21 10:21:14 +0300

    timer clockdow is ready

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,15 @@
 const timerBlock = document.querySelector('.timer__time');
+const deadline = '21 march 2022';
+
+let intervalID;
 
 const format = (num) => {
   return num >= 10 ? num : '0' + num;
 };
 
 const updateClock = () => {
-  // const date = new Date();
-  // const hours = date.getHours(),
-  //   minutes = date.getMinutes(),
-  //   seconds = date.getSeconds();
-
   const date = new Date().getTime();
-  const dateDeadline = new Date('30 march 2022').getTime();
+  const dateDeadline = new Date(deadline).getTime();
   const timeRemaining = (dateDeadline - date) / 1000; // переведем в секунды все оставщееся время
   //получим часы
   const hours = Math.floor(timeRemaining / 60 / 60);
@@ -20,7 +18,13 @@ const updateClock = () => {
   const seconds = Math.floor(timeRemaining % 60);
 
   timerBlock.textContent = `${format(hours)}:${format(minutes)}:${format(seconds)}`;
+
+  if (timeRemaining <= 0) {
+    clearInterval(intervalID);
+    timerBlock.textContent = `00:00:00`;
+  }
 };
+
 updateClock();
 
-const intervalID = setInterval(updateClock, 500); //500 ms  если слабый процессор
+intervalID = setInterval(updateClock, 500); //500 ms  если слабый процессор
