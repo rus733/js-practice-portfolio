42af06948d47de5def5641fd0ba455f50deeab8e
rus733 <9282716617@mail.ru>
2022-03-21 10:08:28 +0300

    create functional Timer clockdow

--- js/timer.js
+++ js/timer.js
@@ -5,13 +5,24 @@ const format = (num) => {
 };
 
 const updateClock = () => {
-  const data = new Date();
-  const hours = data.getHours(),
-    minutes = data.getMinutes(),
-    seconds = data.getSeconds();
+  const date = new Date();
+  const hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds();
 
   timerBlock.textContent = `${format(hours)}:${format(minutes)}:${format(seconds)}`;
 };
 updateClock();
 
 const intervalID = setInterval(updateClock, 500); //500 ms  если слабый процессор
+
+const date = new Date().getTime();
+const dateDeadline = new Date('30 march 2022').getTime();
+const timeRemaining = (dateDeadline - date) / 1000; // переведем в секунды все оставщееся время
+//получим часы
+const hours = Math.floor(timeRemaining / 60 / 60);
+// получим минуты
+const minutes = Math.floor((timeRemaining / 60) % 60); // вынесем из числа всех минут  все  часы применив целочисленное деление на 60
+const seconds = Math.floor(timeRemaining % 60);
+
+console.log('seconds: ', seconds);
