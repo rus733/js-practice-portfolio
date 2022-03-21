008def5768157da112313767f51d8145ce828192
rus733 <9282716617@mail.ru>
2022-03-21 11:25:15 +0300

    remove old code

--- js/scroll.js
+++ js/scroll.js
@@ -18,54 +18,8 @@ const scroll = (block) => {
   });
 };
 
-links.forEach((link) => {
-  scroll(link);
-  // link.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   const id = link.getAttribute('href').substring(1); //отрежем #
-  //   const section = document.getElementById(id); // найдем секцию
-
-  //   if (section) {
-  //     // полифил для плавной  прокрутки  макбуков и сафари
-  //     seamless.scrollIntoView(section, {
-  //       behavior: 'smooth',
-  //       block: 'start',
-  //       inline: 'center',
-  //     });
-  //   }
-  // });
-});
-
-// const scroll = (block) => {
-//   (e) => {
-//     e.preventDefault();
-//     console.log(block);
-//     const id = block.getAttribute('href').substring(1); //отрежем #
-//     const section = document.getElementById(id); // найдем секцию
-
-//     if (section) {
-//       seamless.scrollIntoView(section, {
-//         behavior: 'smooth',
-//         block: 'start',
-//         inline: 'center',
-//       });
-//     }
-//   };
-// };
-
 scroll(scrollBtn);
 
-// scrollBtn.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   console.log(scrollBtn);
-//   const id = scrollBtn.getAttribute('href').substring(1); //отрежем #
-//   const section = document.getElementById(id); // найдем секцию
-
-//   if (section) {
-//     seamless.scrollIntoView(section, {
-//       behavior: 'smooth',
-//       block: 'start',
-//       inline: 'center',
-//     });
-//   }
-// });
+links.forEach((link) => {
+  scroll(link);
+});
