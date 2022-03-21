7d088110796c0be28430ae5a6362179d4d9a63b4
rus733 <9282716617@mail.ru>
2022-03-21 13:37:11 +0300

    days 1 ver

--- js/timer.js
+++ js/timer.js
@@ -4,6 +4,8 @@ const deadline = '27 march 2022';
 
 let intervalID;
 
+const format = (num) => (num >= 10 ? num : `0${num}`);
+
 function wordsForms(value, words) {
   value = Math.abs(value) % 100;
   var num = value % 10;
@@ -12,32 +14,22 @@ function wordsForms(value, words) {
   if (num == 1) return words[0];
   return words[2];
 }
-// wordsForms(value, ['день', 'дня', 'дней']);
-// wordsForms(value, ['час', 'часа', 'часов']);
-// wordsForms(value, ['минута', 'минуты', 'минут']);
-// wordsForms(value, ['секунда', 'секунды', 'секунд']);
-
-const format = (num) => {
-  return num >= 10 ? num : '0' + num;
-};
+// daysForms(['день', 'дня', 'дней']);
+// hoursForms(['час', 'часа', 'часов']);
+// minutesForms(['минута', 'минуты', 'минут']);
+// secondsForms(['секунда', 'секунды', 'секунд']);
 
 const updateClock = () => {
-  const date = new Date().getTime();
+  const date = new Date().getTime(); //текущее время
   const dateDeadline = new Date(deadline).getTime();
-  const timeRemaining = (dateDeadline - date) / 1000; // переведем в секунды все оставщееся время
-
-  //получим часы
-  const hours = Math.floor((timeRemaining / 60 / 60) % 24); // часы не более 24
-  console.log('hours: ', hours);
-  //получим дни
+  const timeRemaining = (dateDeadline - date) / 1000; // остаток времени в сек
+  const hours = Math.floor((timeRemaining / 60 / 60) % 24);
   const days = Math.floor(timeRemaining / 60 / 60 / 24);
-  // получим минуты
-  const minutes = Math.floor((timeRemaining / 60) % 60); // вынесем из числа всех минут  все  часы применив целочисленное деление на 60
+  const minutes = Math.floor((timeRemaining / 60) % 60);
   const seconds = Math.floor(timeRemaining % 60);
 
-  wordsForms(days, ['день', 'дня', 'дней']);
-  daysBlock.textContent = `${format(days)}${wordsForms(days, [' день', ' дня', ' дней'])}`;
-  timerBlock.textContent = `${format(hours)}:${format(minutes)}:${format(seconds)}`; //format(days)
+  daysBlock.textContent = `${format(days)} ${wordsForms(days, ['день', 'дня', 'дней'])}`;
+  timerBlock.textContent = `${format(hours)}:${format(minutes)}:${format(seconds)}`;
 
   if (timeRemaining <= 0) {
     timerBlock.textContent = `00:00:00`;
