c3a2628a1af79e275d99beacc94580b974946efc
rus733 <9282716617@mail.ru>
2022-03-21 10:10:09 +0300

    move clockdow into updateClock

--- js/timer.js
+++ js/timer.js
@@ -5,24 +5,22 @@ const format = (num) => {
 };
 
 const updateClock = () => {
-  const date = new Date();
-  const hours = date.getHours(),
-    minutes = date.getMinutes(),
-    seconds = date.getSeconds();
+  // const date = new Date();
+  // const hours = date.getHours(),
+  //   minutes = date.getMinutes(),
+  //   seconds = date.getSeconds();
+
+  const date = new Date().getTime();
+  const dateDeadline = new Date('30 march 2022').getTime();
+  const timeRemaining = (dateDeadline - date) / 1000; // переведем в секунды все оставщееся время
+  //получим часы
+  const hours = Math.floor(timeRemaining / 60 / 60);
+  // получим минуты
+  const minutes = Math.floor((timeRemaining / 60) % 60); // вынесем из числа всех минут  все  часы применив целочисленное деление на 60
+  const seconds = Math.floor(timeRemaining % 60);
 
   timerBlock.textContent = `${format(hours)}:${format(minutes)}:${format(seconds)}`;
 };
 updateClock();
 
 const intervalID = setInterval(updateClock, 500); //500 ms  если слабый процессор
-
-const date = new Date().getTime();
-const dateDeadline = new Date('30 march 2022').getTime();
-const timeRemaining = (dateDeadline - date) / 1000; // переведем в секунды все оставщееся время
-//получим часы
-const hours = Math.floor(timeRemaining / 60 / 60);
-// получим минуты
-const minutes = Math.floor((timeRemaining / 60) % 60); // вынесем из числа всех минут  все  часы применив целочисленное деление на 60
-const seconds = Math.floor(timeRemaining % 60);
-
-console.log('seconds: ', seconds);
